fcbbaaace174719f6fbb501e3484b45b1b40f597
Mark McEver <mark@fastmail.net>
2015-03-19 01:26:23 -0500

    Simplified attributes references in preparation for further 'confirmation' and 'same' rule changes.

--- src/Mmcev106/LaravelParsleyValidation/Parsley.php
+++ src/Mmcev106/LaravelParsleyValidation/Parsley.php
@@ -7,15 +7,12 @@ class Parsley{
 	private function __construct($validator, $formSelector){
 		$this->validator = $validator;
 		$this->formSelector = $formSelector;
-
-		$attributesByElementName = array();
+		$this->attributesByElementName = array();
 
 		foreach($validator->getRules() as $elementName=>$rules){
-			if(!isset($attributesByElementName[$elementName])){
-				$attributesByElementName[$elementName] = array();
+			if(!isset($this->attributesByElementName[$elementName])){
+				$this->attributesByElementName[$elementName] = array();
 			}
-
-			$attributes =& $attributesByElementName[$elementName];
 
 			foreach($rules as $rule){
 				$parts = explode(':', $rule);
@@ -23,94 +20,93 @@ class Parsley{
 				$value = @$parts[1]; // Only some rules have a value.
 
 				if($name == 'accepted'){
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', '/^(yes|on|1)$/');
+					$this->addAttribute($elementName, $name, 'pattern', '/^(yes|on|1)$/');
 				}
 				else if($name == 'active_url'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-type', 'url');
+					$this->addAttribute($elementName, $name, 'data-parsley-type', 'url');
 				}
 				else if($name == 'alpha'){
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', '/^[A-z]?$/');
+					$this->addAttribute($elementName, $name, 'pattern', '/^[A-z]?$/');
 				}
 				else if($name == 'alpha_dash'){
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', '/^[A-z-_]?$/');
+					$this->addAttribute($elementName, $name, 'pattern', '/^[A-z-_]?$/');
 				}
 				else if($name == 'alpha_num'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-type', 'alphanum');
+					$this->addAttribute($elementName, $name, 'data-parsley-type', 'alphanum');
 				}
 				else if($name == 'between'){
 					// Assume we're working with a string by default.  We'll change this later if this field is determined to be numeric.
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-length', "[$value]");
+					$this->addAttribute($elementName, $name, 'data-parsley-length', "[$value]");
 				}
 				else if($name == 'confirmed'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-equalto', "input[name=" . $elementName . "_confirmation]");
+					$this->addAttribute($elementName, $name, 'data-parsley-equalto', "input[name=" . $elementName . "_confirmation]");
 				}
 				else if($name == 'digits'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-type', 'digits');
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-length', "[$value,$value]");
+					$this->addAttribute($elementName, $name, 'data-parsley-type', 'digits');
+					$this->addAttribute($elementName, $name, 'data-parsley-length', "[$value,$value]");
 				}
 				else if($name == 'digits_between'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-type', 'digits');
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-length', "[$value]");
+					$this->addAttribute($elementName, $name, 'data-parsley-type', 'digits');
+					$this->addAttribute($elementName, $name, 'data-parsley-length', "[$value]");
 				}
 				else if($name == 'email'){
-					$this->addAttribute($attributes, $elementName, $name, 'type', 'email');
+					$this->addAttribute($elementName, $name, 'type', 'email');
 				}
 				else if($name == 'in'){
 					$value = str_replace(',', '|', $value);
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', "/^($value)$/");
+					$this->addAttribute($elementName, $name, 'pattern', "/^($value)$/");
 				}
 				else if($name == 'integer'){
-					$this->addAttribute($attributes, $elementName, $name, 'type', 'number');
+					$this->addAttribute($elementName, $name, 'type', 'number');
 				}
 				else if($name == 'max'){
 					// Assume we're working with a string by default.  We'll change this later if this field is determined to be numeric.
-					$this->addAttribute($attributes, $elementName, $name, 'maxlength', $value);
+					$this->addAttribute($elementName, $name, 'maxlength', $value);
 				}
 				else if($name == 'min'){
 					// Assume we're working with a string by default.  We'll change this later if this field is determined to be numeric.
-					$this->addAttribute($attributes, $elementName, $name, 'minlength', $value);
+					$this->addAttribute($elementName, $name, 'minlength', $value);
 				}
 				else if($name == 'not_in'){
 					$value = str_replace(',', '|', $value);
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', '/^(?!(one|two|three)$)/');
+					$this->addAttribute($elementName, $name, 'pattern', '/^(?!(one|two|three)$)/');
 				}
 				else if($name == 'numeric'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-type', 'number');
+					$this->addAttribute($elementName, $name, 'data-parsley-type', 'number');
 				}
 				else if($name == 'regex'){
-					$this->addAttribute($attributes, $elementName, $name, 'pattern', $value);
+					$this->addAttribute($elementName, $name, 'pattern', $value);
 				}
 				else if($name == 'required'){
-					$this->addAttribute($attributes, $elementName, $name, 'required', '');
+					$this->addAttribute($elementName, $name, 'required', '');
 				}
 				else if($name == 'size'){
 					// Assume we're working with a string by default.  We'll change this later if this field is determined to be numeric.
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-length', "[$value,$value]");
+					$this->addAttribute($elementName, $name, 'data-parsley-length', "[$value,$value]");
 				}
 				else if($name == 'same'){
-					$this->addAttribute($attributes, $elementName, $name, 'data-parsley-equalto', "input[name=$value]");
+					$this->addAttribute($elementName, $name, 'data-parsley-equalto', "input[name=$value]");
 				}
 				else if($name == 'url'){
-					$this->addAttribute($attributes, $elementName, $name, 'type', 'url');
+					$this->addAttribute($elementName, $name, 'type', 'url');
 				}
 			}
 
-			if($this->isElementNumeric($attributes)){
+			if($this->isElementNumeric($elementName)){
 				// Laravel assumes fields are strings by default, so we create string related Parsley rules by default.
 				// If a rule is exists for a given field specifying it as numeric in Laravel, we must change the string related validators in Parsley to their number related equivalents.
-				$this->switchStringValidatorsToNumberValidators($attributes);
+				$this->switchStringValidatorsToNumberValidators($elementName);
 			}
 		}
-
-		$this->attributesByElementName = $attributesByElementName;
 	}
 
 	static function buildJS($validator, $formSelector='form'){
 		$parsley = new Parsley($validator, $formSelector);
-		return $parsley->buildJSForAttributes($formSelector, $formSelector, $parsley->attributesByElementName);
+		return $parsley->buildJSForAttributes($formSelector, $formSelector);
 	}
 
-	private function addAttribute(&$attributes, $elementName, $ruleName, $attributeName, $attributeValue){
+	private function addAttribute($elementName, $ruleName, $attributeName, $attributeValue){
+		$attributes =& $this->attributesByElementName[$elementName];
 
 		if($attributeName == 'pattern'){
 			// Escape backslashes so they'll end up in the final javascript regex.
@@ -137,11 +133,14 @@ class Parsley{
 		}
 	}
 
-	private function isElementNumeric($attributes){
+	private function isElementNumeric($elementName){
+		$attributes =& $this->attributesByElementName[$elementName];
 		return @$attributes['type'] == 'number' || @$attributes['data-parsley-type'] == 'number';
 	}
 
-	private function switchStringValidatorsToNumberValidators(&$attributes){
+	private function switchStringValidatorsToNumberValidators($elementName){
+		$attributes =& $this->attributesByElementName[$elementName];
+
 		if(isset($attributes['data-parsley-length'])){
 			$attributes['data-parsley-range'] = $attributes['data-parsley-length'];
 			unset($attributes['data-parsley-length']);
@@ -158,12 +157,12 @@ class Parsley{
 		}
 	}
 
-	private static function buildJSForAttributes($formSelector, $formSelector, $attributesByElementName){
+	private function buildJSForAttributes($formSelector, $formSelector){
 		$js = PHP_EOL; // Start the js with an EOL so the beginning and ending script tags line up (regardless of the indentation level where the JS is included).
 		$js .= "<script>" . PHP_EOL;
 		$js .= "  $(function(){" . PHP_EOL;
 
-		foreach($attributesByElementName as $elementName=>$attributes){
+		foreach($this->attributesByElementName as $elementName=>$attributes){
 			foreach($attributes as $name=>$value){
 				if($value == NULL){
 					$value = '';
